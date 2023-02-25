3afad1ce32bdd0922dee9f95b30bb3b9550d2d5b
Eduardo Fukushima <eduardo.fukushima@gmail.com>
2023-02-25 15:46:37 -0300

    Cap 4 Fiap

--- DesignPatterns/src/br/com/fiap/patterns/model/Pis.java
+++ DesignPatterns/src/br/com/fiap/patterns/model/Pis.java
@@ -2,11 +2,19 @@ package br.com.fiap.patterns.model;
 
 import java.util.Observable;
 
+import br.com.fiap.patterns.config.ConfigSingleton;
+
 public class Pis extends Observable implements Imposto {
 
-	private float aliquota = 0.6f;
+	private float aliquota;
 
+	
 	private float valorPis;
+	
+	public Pis() {
+		aliquota = Float.parseFloat(
+				ConfigSingleton.getInstance().getProperty("aliquota"));
+	}
 
 	@Override
 	public void calcularImposto(float valor) { 
